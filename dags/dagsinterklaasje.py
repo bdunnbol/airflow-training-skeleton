60e21656afa9f37d05ef96feaeccd04aff29267b
Bas Dunn <bdunn@bol.com>
2019-05-28 13:12:48 +0200

    add dag2

---
+++ dags/dagsinterklaasje.py
@@ -0,0 +1,34 @@
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+dag = DAG(
+    dag_id="dagsinterklaasje",
+    default_args={
+        "owner": "naamhierinvullen",
+        "start_date": airflow.utils.dates.days_ago(3),
+    },
+)
+
+t1 = BashOperator(
+    task_id="print_exec_date", bash_command="echo {{ execution_date }}", dag=dag
+)
+
+t2 = BashOperator(
+    task_id="wait_5", bash_command="sleep 5", dag=dag
+)
+
+t3 = BashOperator(
+    task_id="wait_1", bash_command="sleep 1", dag=dag
+)
+
+t4 = BashOperator(
+    task_id="wait_10", bash_command="sleep 10", dag=dag
+)
+
+t5 = DummyOperator(
+    task_id="the_end", dag=dag
+)
+
+t1 >> [t2, t3, t4] >> t5
