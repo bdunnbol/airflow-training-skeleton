cbe5c0bbf01d504055a004ea4cb605cbedda8d66
Bas Dunn <bdunn@bol.com>
2019-05-29 11:42:02 +0200

    add Dag Bob

--- dags/superjob.py
+++ dags/superjob.py
@@ -11,43 +11,58 @@ from airflow_training.operators.postgres_to_gcs import (
 from airflow.operators import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from airflow.hooks.http_hook import HttpHook
-import json
+from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
+
 
 class MyOwnOperator(BaseOperator):
 
-    ui_color = '#0090e3'
-    ui_fgcolor = '#000000'
+    ui_color = "#0090e3"
+    ui_fgcolor = "#000000"
 
     @apply_defaults
-    def __init__(self, endpoint, *args, **kwargs):
+    def __init__(
+        self,
+        http_endpoint,
+        http_connection_id,
+        gcs_connection_id,
+        gcs_bucket,
+        gcs_filename,
+        *args,
+        **kwargs
+    ):
         super().__init__(*args, **kwargs)
-        self.endpoint = endpoint
+        self.http_endpoint = http_endpoint
+        self.http_connection_id = http_connection_id
+        self.gcs_connection_id = gcs_connection_id
+        self.gcs_bucket = gcs_bucket
+        self.gcs_filename = gcs_filename
 
     def execute(self, context):
         print("exec")
         self._download_from_http()
+        self._upload_to_gcs()
 
-    # def _upload_to_gcs(self):
-    #     """
-    #     Upload all of the file splits (and optionally the schema .json file) to
-    #     Google Cloud Storage.
-    #     """
-    #     hook = GoogleCloudStorageHook(
-    #         google_cloud_storage_conn_id=self.google_cloud_storage_conn_id,
-    #         delegate_to=self.delegate_to,
-    #     )
-    #     for object, tmp_file_handle in files_to_upload.items():
-    #         hook.upload(self.bucket, object, tmp_file_handle.name, "application/json")
+    def _upload_to_gcs(self):
+        """
+        Upload all of the file splits (and optionally the schema .json file) to
+        Google Cloud Storage.
+        """
+        hook = GoogleCloudStorageHook(
+            google_cloud_storage_conn_id=self.gcs_connection_id
+        )
+
+        hook.upload(
+            self.gcs_bucket, "imthecontent", self.gcs_filename, "application/json"
+        )
 
     def _download_from_http(self):
-        http = HttpHook('GET', http_conn_id='http_exchangerate')
+        http = HttpHook("GET", http_conn_id=self.http_connection_id)
         self.log.info("Calling HTTP method")
-        self.log.info(self.endpoint + " is the endpoint")
-        response = http.run("/airflow-training-transform-valutas?date=2018-01-01&to=EUR")
-        self.log.info(response)
+        response = http.run(self.http_endpoint)
         self.log.info(response.text)
-        self.log.info(json.loads(response.text))
+
         return response.text
+
 
 dag = DAG(
     dag_id="superjob",
@@ -69,7 +84,15 @@ pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
     dag=dag,
 )
 
-myown = MyOwnOperator(task_id='myown', dag=dag, endpoint="https://www.google.com")
+myown = MyOwnOperator(
+    task_id="myown",
+    dag=dag,
+    http_connection_id="http_exchangerate",
+    http_endpoint="/airflow-training-transform-valutas?date={{  ds  }}&to=EUR",
+    gcs_connection_id="google_cloud_default",
+    gcs_bucket="een_emmer",
+    gcs_filename="exchangerate_{{ds}}",
+)
 
 
 myown >> pgsl_to_gcs
